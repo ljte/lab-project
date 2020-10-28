fc6d8fa1bf26eb6989de7576715014c13765808b
ljte <ljte823@gmail.com>
2020-10-28 23:56:30 +0300

    tests

--- department_app/tests/test_department_views.py
+++ department_app/tests/test_department_views.py
@@ -36,106 +36,110 @@ class TestViews(unittest.TestCase):
 
     def test_main_departments_page(self):
         """test that the department's index page loads correctly"""
-        page = self.tester.get('/')
+        with self.context():
+            page = self.tester.get('/')
 
-        assert page.status_code == 200
-        assert b'Filter by the average salary' in page.data
+            assert page.status_code == 200
+            assert b'Filter by the average salary' in page.data
 
     def test_correct_search_view(self):
         """test that search function works correctly"""
-        response = self.tester.get('/departments/search', data={'search_string': 'Finance'})
+        with self.context():
+            response = self.tester.get('/departments/search', data={'search_string': 'Finance'})
 
-        assert response.status_code == 200
+            assert response.status_code == 200
 
-        assert b'Finance department' in response.data
-        assert b'Human Resource department' not in response.data
-        assert b'Accounting department' not in response.data
+            assert b'Finance department' in response.data
+            assert b'Human Resource department' not in response.data
+            assert b'Accounting department' not in response.data
 
     def test_incorrect_search_view(self):
         """test that search function works correctly"""
-        response = self.tester.get('/departments/search', data={'search_string': 12312})
+            with self.context():
+            response = self.tester.get('/departments/search', data={'search_string': 12312})
 
-        assert response.status_code == 200
+            assert response.status_code == 200
 
-        assert b'Finance department' not in response.data
-        assert b'Human Resource department' not in response.data
-        assert b'Accounting department' not in response.data
+            assert b'Finance department' not in response.data
+            assert b'Human Resource department' not in response.data
+            assert b'Accounting department' not in response.data
 
     def test_edit_request(self):
         """test that editing a department works correctly"""
         with self.context():
             dep = utils.get_or_404(Department, name='Finance department')
-        response = self.tester.post(f'/departments/edit/{dep.id}',
-                                    data={'name': 'Updated department'},
-                                    follow_redirects=True)
-        assert b'Updated department' in response.data
-        assert b'>Finance'not in response.data
+            response = self.tester.post(f'/departments/edit/{dep.id}',
+                                        data={'name': 'Updated department'},
+                                        follow_redirects=True)
+            assert b'Updated department' in response.data
+            assert b'>Finance'not in response.data
 
-        self.tester.post(f'/departments/edit/{dep.id}',
-                         data={'name': 'Finance department'},
-                         follow_redirects=True)
+            self.tester.post(f'/departments/edit/{dep.id}',
+                             data={'name': 'Finance department'},
+                             follow_redirects=True)
 
     def test_incorrect_edit_request(self):
         """test that editing a department works correctly"""
         with self.context():
             dep = utils.get_or_404(Department, name='Finance department')
-        response = self.tester.post(f'/departments/edit/{dep.id}',
-                                    data={'name': 1231},
-                                    follow_redirects=True)
-        assert b"invalid department&#39;s name" in response.data
+            response = self.tester.post(f'/departments/edit/{dep.id}',
+                                        data={'name': 1231},
+                                        follow_redirects=True)
+            assert b"invalid department&#39;s name" in response.data
 
-        response = self.tester.post(f'/departments/edit/{dep.id}',
-                                    data={'name': 'somedepartment'},
-                                    follow_redirects=True)
-        assert b"invalid department&#39;s name" in response.data
+            response = self.tester.post(f'/departments/edit/{dep.id}',
+                                        data={'name': 'somedepartment'},
+                                        follow_redirects=True)
+            assert b"invalid department&#39;s name" in response.data
 
     def test_delete_view(self):
         """test that deleting a department works correctly"""
         with self.context():
             utils.insert_into_db(Department(name='Delete department'))
             dep = utils.get_or_404(Department, name='Delete department')
-        response = self.tester.get(f'/departments/delete/{dep.id}', follow_redirects=True)
+            response = self.tester.get(f'/departments/delete/{dep.id}', follow_redirects=True)
 
-        assert b'>Delete department' not in response.data
-        assert b'Succesfully deleted' in response.data
+            assert b'>Delete department' not in response.data
+            assert b'Succesfully deleted' in response.data
 
-        response = self.tester.get('/departments/delete/44252312', follow_redirects=True)
+            response = self.tester.get('/departments/delete/44252312', follow_redirects=True)
 
-        assert b'was not found' in response.data
+            assert b'was not found' in response.data
 
     def test_post_view(self):
         """test that posting a new department works correctly"""
-        response = self.tester.post('/departments/add', data={'name': 'New department'},
-                                    follow_redirects=True)
-
-        assert b'Successfully added' in response.data
-        assert b'New department' in response.data
-
         with self.context():
+            response = self.tester.post('/departments/add', data={'name': 'New department'},
+                                        follow_redirects=True)
+
+            assert b'Successfully added' in response.data
+            assert b'New department' in response.data
+
             dep = utils.get_or_404(Department, name='New department')
             utils.delete_from_db(dep)
 
-        response = self.tester.post('/departments/add', data={'name': 123},
-                                    follow_redirects=True)
-        assert b"invalid department&#39;s name" in response.data
+            response = self.tester.post('/departments/add', data={'name': 123},
+                                        follow_redirects=True)
+            assert b"invalid department&#39;s name" in response.data
 
     def test_filter_by_avg_salary_department(self):
         """test that filtering departments by avg salary works correctly"""
-        response = self.tester.post('/departments/filter_by_salary', data={'comparison': '>',
-                                                                           'average_salary': 650})
+        with self.context():
+            response = self.tester.post('/departments/filter_by_salary', data={'comparison': '>',
+                                                                               'average_salary': 650})
 
-        assert b'Accounting department' in response.data
-        assert b'Finance department' not in response.data
-        assert b'Human Resource department' not in response.data
+            assert b'Accounting department' in response.data
+            assert b'Finance department' not in response.data
+            assert b'Human Resource department' not in response.data
 
-        response = self.tester.post('/departments/filter_by_salary',
-                                    data={'comparison': '>',
-                                          'average_salary': 'agas'},
-                                    follow_redirects=True)
-        assert b'Invalid average salary' in response.data
+            response = self.tester.post('/departments/filter_by_salary',
+                                        data={'comparison': '>',
+                                              'average_salary': 'agas'},
+                                        follow_redirects=True)
+            assert b'Invalid average salary' in response.data
 
-        response = self.tester.post('/departments/filter_by_salary',
-                                    data={'comparison': 'asdg',
-                                          'average_salary': 123},
-                                    follow_redirects=True)
-        assert b'Invalid comparison operator' in response.data
+            response = self.tester.post('/departments/filter_by_salary',
+                                        data={'comparison': 'asdg',
+                                              'average_salary': 123},
+                                        follow_redirects=True)
+            assert b'Invalid comparison operator' in response.data
--- department_app/tests/test_employee_views.py
+++ department_app/tests/test_employee_views.py
@@ -37,68 +37,72 @@ class TestViews(unittest.TestCase):
 
     def test_main_employees_page(self):
         """test that /employees page loads correctly"""
-        response = self.tester.get('/employees')
+        with cls.context():
+            response = self.tester.get('/employees')
 
-        assert b'Filter by birthday' in response.data
-        assert b'Filter by department' in response.data
+            assert b'Filter by birthday' in response.data
+            assert b'Filter by department' in response.data
 
     def test_search_employee(self):
         """test that searching an employees by fullname works correctly"""
-        response = self.tester.get('/employees/search', data={'search_string': 'Andrey'})
+        with cls.context():
+            response = self.tester.get('/employees/search', data={'search_string': 'Andrey'})
 
-        assert b'Andrey Bobrov' in response.data
-        assert b'Anna Volkova' not in response.data
-        assert b'Boris Nemchenko' not in response.data
-        assert b'Vladimir Novikov' not in response.data
+            assert b'Andrey Bobrov' in response.data
+            assert b'Anna Volkova' not in response.data
+            assert b'Boris Nemchenko' not in response.data
+            assert b'Vladimir Novikov' not in response.data
 
     def test_filter_by_department(self):
         """test filtering employees by department """
-        response = self.tester.post('/employees/filter_by_department', data={'department': 'Marketing department'})
+        with cls.context():
+            response = self.tester.post('/employees/filter_by_department', data={'department': 'Marketing department'})
 
-        assert b'Boris Nemchenko' in response.data
-        assert b'Vladimir Novikov' in response.data
-        assert b'Andrey Bobrov' not in response.data
-        assert b'Anna Volkova' not in response.data
+            assert b'Boris Nemchenko' in response.data
+            assert b'Vladimir Novikov' in response.data
+            assert b'Andrey Bobrov' not in response.data
+            assert b'Anna Volkova' not in response.data
 
     def test_filter_by_bday(self):
         """test filtering employees by birthday"""
-        response = self.tester.post('/employees/filter_by_bday', data={'bday': '1992-06-23'})
+        with cls.context():
+            response = self.tester.post('/employees/filter_by_bday', data={'bday': '1992-06-23'})
 
-        assert b'Andrey Bobrov' in response.data
-        assert b'Anna Volkova' not in response.data
-        assert b'Boris Nemchenko' not in response.data
-        assert b'Vladimir Novikov' not in response.data
+            assert b'Andrey Bobrov' in response.data
+            assert b'Anna Volkova' not in response.data
+            assert b'Boris Nemchenko' not in response.data
+            assert b'Vladimir Novikov' not in response.data
 
-        response = self.tester.post('/employees/filter_by_bday', data={'bday': 1231}, follow_redirects=True)
+            response = self.tester.post('/employees/filter_by_bday', data={'bday': 1231}, follow_redirects=True)
 
-        assert b'Birthday must have YY-MM-DD format' in response.data
+            assert b'Birthday must have YY-MM-DD format' in response.data
 
-        response = self.tester.post('/employees/filter_by_bday', follow_redirects=True)
+            response = self.tester.post('/employees/filter_by_bday', follow_redirects=True)
 
-        assert b'You must specify both start and end dates or choose a birthday' in response.data
+            assert b'You must specify both start and end dates or choose a birthday' in response.data
 
     def test_filter_by_date_period(self):
         """test getting those employees whose birthday fall into
         the date period from start_date to end_date"""
+        with cls.context():
+            response = self.tester.post('/employees/filter_by_bday', data={'start_date': '1992-01-01',
+                                                                           'end_date': '1993-12-31'})
 
-        response = self.tester.post('/employees/filter_by_bday', data={'start_date': '1992-01-01',
-                                                                       'end_date': '1993-12-31'})
+            assert b'Andrey Bobrov' in response.data
+            assert b'Boris Nemchenko' in response.data
+            assert b'Anna Volkova' not in response.data
+            assert b'Vladimir Novikov' not in response.data
 
-        assert b'Andrey Bobrov' in response.data
-        assert b'Boris Nemchenko' in response.data
-        assert b'Anna Volkova' not in response.data
-        assert b'Vladimir Novikov' not in response.data
+            response = self.tester.post('/employees/filter_by_bday', data={'start_date': 124,
+                                                                           'end_date': '1993-12-31'}, follow_redirects=True)
+            assert b'Dates must have YY-MM-DD format' in response.data
 
-        response = self.tester.post('/employees/filter_by_bday', data={'start_date': 124,
-                                                                       'end_date': '1993-12-31'}, follow_redirects=True)
-        assert b'Dates must have YY-MM-DD format' in response.data
+            response = self.tester.post('/employees/filter_by_bday', data={'start_date': '1992-01-01',
+                                                                           'end_date': 12331}, follow_redirects=True)
+            assert b'Dates must have YY-MM-DD format' in response.data
 
-        response = self.tester.post('/employees/filter_by_bday', data={'start_date': '1992-01-01',
-                                                                       'end_date': 12331}, follow_redirects=True)
-        assert b'Dates must have YY-MM-DD format' in response.data
-
-        response = self.tester.post('/employees/filter_by_bday', follow_redirects=True)
-        assert b'You must specify both start and end dates or choose a birthday' in response.data
+            response = self.tester.post('/employees/filter_by_bday', follow_redirects=True)
+            assert b'You must specify both start and end dates or choose a birthday' in response.data
 
     def test_edit_employee(self):
         """test that editing an employee works correctly"""
@@ -108,86 +112,84 @@ class TestViews(unittest.TestCase):
                                           bday=date(1997, 12, 15), department_id=dep.id))
             emp = utils.get_or_404(Employee, fullname='Edit Employee')
 
-        response = self.tester.post(f'/employees/edit/{emp.id}',
-                                    data={'fullname': 'Updated Employee'},
-                                    follow_redirects=True)
-        assert b'Successfully updated' in response.data
-        assert b'Updated Employee' in response.data
+            response = self.tester.post(f'/employees/edit/{emp.id}',
+                                        data={'fullname': 'Updated Employee'},
+                                        follow_redirects=True)
+            assert b'Successfully updated' in response.data
+            assert b'Updated Employee' in response.data
 
-        response = self.tester.post(f'/employees/edit/{emp.id}',
-                                    data={'fullname': 123},
-                                    follow_redirects=True)
-        assert b"invalid employee&#39;s fullname" in response.data
+            response = self.tester.post(f'/employees/edit/{emp.id}',
+                                        data={'fullname': 123},
+                                        follow_redirects=True)
+            assert b"invalid employee&#39;s fullname" in response.data
 
-        response = self.tester.post(f'/employees/edit/{emp.id}',
-                                    data={'salary': 'asfd'},
-                                    follow_redirects=True)
-        assert b"Failed to update" in response.data
+            response = self.tester.post(f'/employees/edit/{emp.id}',
+                                        data={'salary': 'asfd'},
+                                        follow_redirects=True)
+            assert b"Failed to update" in response.data
 
-        response = self.tester.post(f'/employees/edit/{emp.id}',
-                                    data={'department': 1231},
-                                    follow_redirects=True)
-        assert b"Failed to update" in response.data
+            response = self.tester.post(f'/employees/edit/{emp.id}',
+                                        data={'department': 1231},
+                                        follow_redirects=True)
+            assert b"Failed to update" in response.data
 
-        response = self.tester.post(f'/employees/edit/{emp.id}',
-                                    data={'bday': 1231},
-                                    follow_redirects=True)
-        assert b"Failed to update" in response.data
+            response = self.tester.post(f'/employees/edit/{emp.id}',
+                                        data={'bday': 1231},
+                                        follow_redirects=True)
+            assert b"Failed to update" in response.data
 
-        with self.context():
             utils.delete_from_db(emp)
 
     def test_post_employee(self):
         """test that posting a new employee work well"""
-        response = self.tester.post('/employees/add', data={
-            'fullname': 'Post Employee',
-            'salary': 312,
-            'bday': date(1995, 12, 31),
-            'dep_name': 'Marketing department'
-        }, follow_redirects=True)
-
-        assert b'Successfully added' in response.data
-        assert b'Post Employee' in response.data
-
         with self.context():
+            response = self.tester.post('/employees/add', data={
+                'fullname': 'Post Employee',
+                'salary': 312,
+                'bday': date(1995, 12, 31),
+                'dep_name': 'Marketing department'
+            }, follow_redirects=True)
+
+            assert b'Successfully added' in response.data
+            assert b'Post Employee' in response.data
+
             emp = utils.get_or_404(Employee, fullname='Post Employee')
 
-        assert int(emp.salary) == 312
-        assert emp.bday == date(1995, 12, 31)
+            assert int(emp.salary) == 312
+            assert emp.bday == date(1995, 12, 31)
 
-        response = self.tester.post('/employees/add', data={
-            'fullname': 124,
-            'salary': 312,
-            'bday': date(1995, 12, 31),
-            'dep_name': 'Marketing department'
-        }, follow_redirects=True)
-        assert b"invalid employee&#39;s name" in response.data
+            response = self.tester.post('/employees/add', data={
+                'fullname': 124,
+                'salary': 312,
+                'bday': date(1995, 12, 31),
+                'dep_name': 'Marketing department'
+            }, follow_redirects=True)
+            assert b"invalid employee&#39;s name" in response.data
 
-        response = self.tester.post('/employees/add', data={
-            'fullname': 'New Employee',
-            'salary': 'fas',
-            'bday': date(1995, 12, 31),
-            'dep_name': 'Marketing department'
-        }, follow_redirects=True)
-        assert b'could not convert' in response.data
+            response = self.tester.post('/employees/add', data={
+                'fullname': 'New Employee',
+                'salary': 'fas',
+                'bday': date(1995, 12, 31),
+                'dep_name': 'Marketing department'
+            }, follow_redirects=True)
+            assert b'could not convert' in response.data
 
-        response = self.tester.post('/employees/add', data={
-            'fullname': 'New Employee',
-            'salary': 124,
-            'bday': 1241,
-            'dep_name': 'Marketing department'
-        }, follow_redirects=True)
-        assert b'does not match format &#39;%Y-%m-%d&#39;' in response.data
+            response = self.tester.post('/employees/add', data={
+                'fullname': 'New Employee',
+                'salary': 124,
+                'bday': 1241,
+                'dep_name': 'Marketing department'
+            }, follow_redirects=True)
+            assert b'does not match format &#39;%Y-%m-%d&#39;' in response.data
 
-        response = self.tester.post('/employees/add', data={
-            'fullname': 'New Employee',
-            'salary': 124,
-            'bday': date(1995, 12, 31),
-            'dep_name': 312
-        }, follow_redirects=True)
-        assert b'was not found' in response.data
+            response = self.tester.post('/employees/add', data={
+                'fullname': 'New Employee',
+                'salary': 124,
+                'bday': date(1995, 12, 31),
+                'dep_name': 312
+            }, follow_redirects=True)
+            assert b'was not found' in response.data
 
-        with self.context():
             utils.delete_from_db(emp)
 
     def test_delete_employee(self):
@@ -198,9 +200,9 @@ class TestViews(unittest.TestCase):
                                           bday=date(1994, 12, 12), department_id=dep.id))
             emp = utils.get_or_404(Employee, fullname='Delete Employee')
 
-        response = self.tester.get('/employees')
-        assert b'>Delete Employee' in response.data
+            response = self.tester.get('/employees')
+            assert b'>Delete Employee' in response.data
 
-        response = self.tester.get(f'/employees/delete/{emp.id}', follow_redirects=True)
-        assert b'>Delete Employee' not in response.data
-        assert b'Succesfully deleted' in response.data
+            response = self.tester.get(f'/employees/delete/{emp.id}', follow_redirects=True)
+            assert b'>Delete Employee' not in response.data
+            assert b'Succesfully deleted' in response.data
