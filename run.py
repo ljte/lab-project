42759f8c41a4361409b9e16c08dc1fade62a9d96
ljte <ljte823@gmail.com>
2020-10-29 17:29:17 +0300

    travis

--- run.py
+++ run.py
@@ -2,16 +2,18 @@ from department_app import create_app
 from department_app.config import TestConfig
 
 
-app = create_app()
-
-
-def run():
+def run(test=False):
     """method to run gunicorn"""
-    app = create_app()
+    if test:
+        app = create_app(app_config=TestConfig)
+    else:
+        app = create_app()
+
     return app
 
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
 
 
