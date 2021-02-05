6e3806a95da2d89cefea0d09f9f02bb0c41eadb7
ljte <ljte823@gmail.com>
2021-02-05 12:12:46 +0300

    remove all files that is gonna be reworked

--- department_app/static/disableButtons.js
+++
@@ -1,8 +0,0 @@
-let buttons = document.querySelectorAll(".btn");
-for (button of buttons) {
-  button.addEventListener('click', (click) => {
-      // disable clicked button so the user can not send the same request
-      // set timeout so the form data can be send to the view function
-      setTimeout(() => click.target.disabled = true, 0.1);
-  })
-}
--- department_app/views/__init__.py
+++
@@ -1,4 +0,0 @@
-"""view handlers"""
-
-from department_app.views.department_views import department_bp
-from department_app.views.employees_views import employees_bp
