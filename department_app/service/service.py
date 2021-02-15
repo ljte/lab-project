7a12d51257e65c98c41e4378b021f37b32042d0b
Dima Orgish <dzmitry_orgish@epam.com>
2021-02-15 20:37:05 +0300

    add crud

--- department_app/service/service.py
+++ department_app/service/service.py
@@ -1,8 +1,10 @@
 from typing import Union, List, Mapping, Any
 
 from loguru import logger
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 
 from department_app.domain.interfaces import IDatabase
+from department_app.domain.exceptions import RecordAlreadyExistsError
 from department_app.database import Base
 
 
@@ -11,22 +13,52 @@ class DatabaseService:
     def __init__(self, database: IDatabase):
         self.db = database
 
-    def all(self, model: Base):
+    def all(self, model: Base) -> List[Base]:
         with self.db.session() as s:
             try:
                 records = s.query(model).all()
-            except Exception as e:
-                logger.exception(e)
+            except SQLAlchemyError:
                 s.rollback()
-            else:
-                return records
+                raise
+            return records
 
-    def get(self, model: Base, **criterion: Mapping[str, Any]):
+    def get(self, model: Base, **criterion: Mapping[str, Any]) -> Base:
         with self.db.session() as s:
             try:
                 record = s.query(model).filter_by(**criterion).first()
-            except Exception as e:
-                logger.exception(e)
+            except SQLAlchemyError:
                 s.rollback()
-            else:
-                return record
+                raise
+            return record or None
+
+    def insert(self, record: Base) -> None:
+        with self.db.session() as s :
+            try:
+                s.add(record)
+                s.commit()
+            except IntegrityError as e:
+                raise RecordAlreadyExistsError(record) from e
+            except SQLAlchemyError:
+                raise
+            finally:
+                s.rollback()
+
+    def update(self, record: Base, **updated_fieds: Mapping[str, Any]) -> None:
+        with self.db.session() as s:
+            try:
+                s.query(type(record)).\
+                        filter_by(id = record.id).\
+                        update(updated_fieds)
+                s.commit()
+            except SQLAlchemyError:
+                s.rollback()
+                raise
+
+    def delete(self, record: Base) -> None:
+        with self.db.session() as s:
+            try:
+                s.delete(record)
+                s.commit()
+            except SQLAlchemyError:
+                s.rollback()
+                raise
