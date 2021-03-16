ac265f3517657b4308c6ec8962cb37273b095863
ljte <ljte823@gmail.com>
2021-03-16 23:09:56 +0300

    add range bday filter

--- department_app/backend/views/employee_api.py
+++ department_app/backend/views/employee_api.py
@@ -10,13 +10,18 @@ class EmployeeView(APIResource):
 
     def get_queryset(self):
         query_dict = self.request.GET.dict()
-        print(query_dict)
         pattern = query_dict.get("search_pattern", "")
         department_name = query_dict.get("department_name", "")
-        bday = query_dict.get("bday", "")
+        start_date = query_dict.get("start_date", "")
+        end_date = query_dict.get("end_date", "")
+        if start_date and end_date:
+            return get_all(
+                self.model,
+                department__name__contains=department_name,
+                bday__range=[start_date, end_date],
+            )
         return get_all(
             self.model,
             fullname__icontains=pattern,
             department__name__contains=department_name,
-            bday__contains=bday,
         )
