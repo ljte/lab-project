81b5e84a94bfc9d4d88c08da54d649d15272885b
Dima Orgish <dzmitry_orgish@epam.com>
2021-03-16 20:38:55 +0300

    add bday filter

--- department_app/backend/views/employee_api.py
+++ department_app/backend/views/employee_api.py
@@ -10,10 +10,13 @@ class EmployeeView(APIResource):
 
     def get_queryset(self):
         query_dict = self.request.GET.dict()
+        print(query_dict)
         pattern = query_dict.get("search_pattern", "")
         department_name = query_dict.get("department_name", "")
+        bday = query_dict.get("bday", "")
         return get_all(
             self.model,
             fullname__icontains=pattern,
             department__name__contains=department_name,
+            bday__contains=bday,
         )
